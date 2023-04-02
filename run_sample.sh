6c4931fd6c68bdf12ea3624525da68783c7bcd0a
Adam B <13562139+catenocrypt@users.noreply.github.com>
2023-04-03 00:18:36 +0200

    Add simple CI

---
+++ run_sample.sh
@@ -0,0 +1,11 @@
+cargo run generate
+
+cargo run
+
+echo oil oil oil oil oil oil oil oil oil oil oil oil | cargo run import
+
+cargo run inv
+
+cargo run invprev
+
+cargo run verify npub195z34nrkuggzmp0dve4ugs85fdkq5q34n3yxaw98zj7afq9cnp2sncwkch npub1vfxu6eandjw8ufpe0wqhhrr4q86z65777vrs66jvx49l2a5fnyvsycs248 npub1en8lkh8c5ljfgjqutpd6q2ht9wy05psguepr7n6958e8yjh0p7wqg3psj9
